7aabaa8d1b9fcabdef7123807f365b761ba56788
Matias-agustin97 <matias.a.acuervo@gmail.com>
2021-11-09 10:05:35 -0300

    lets do this

---
+++ src/main/java/com/example/amigoscodetuto/AmigoscodetutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.amigoscodetuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodetutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoscodetutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amigoscodetuto/AmigoscodetutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amigoscodetuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodetutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
